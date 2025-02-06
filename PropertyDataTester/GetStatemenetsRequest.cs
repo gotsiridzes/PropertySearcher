9e44fe8df8ce5af668bec819e8989149cc81db4c
Saba Gotsiridze <sag@leneo.io>
2025-02-06 16:39:38 +0400

    Create a basic implementation for statement searching.

---
+++ PropertyDataTester/GetStatemenetsRequest.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace PropertyDataTester;
+
+public sealed record GetStatementsRequest(List<DealType> DealTypes, List<RealEstateType> RealEstateTypes, List<Districts> Districts);
+
+public enum DealType
+{
+	Sale = 1,
+	Rent = 2,
+	Collateral = 3,
+	DailyRent = 7
+}
+
+public enum RealEstateType
+{
+	Flat = 1,
+	House = 2,
+	Cottage = 3,
+	Land = 4,
+	Commercial = 5,
+	Hotel
+}
+
+public enum Districts
+{
+	GldaniNadzaladevi = 1,
+	AroundTbilisi = 2,
+	DidubeChugureti = 3,
+	VakeSaburtalo = 4,
+	IsaniSamgori = 5,
+	OldTbilisi = 6
+}
+
+public enum Currency
+{
+	Gel = 1,
+	Usd = 2
+}
+
+public static class QueryStringConverter
+{
+	private static readonly Dictionary<string, string> FieldMappings = new()
+	{
+		{ "DealTypes", "deal_types" },
+		{ "RealEstateTypes", "real_estate_type" },
+		{ "Districts", "districts" },
+		//{"Currency", "currency_id"}
+	};
+
+	public static string ToQueryString(this GetStatementsRequest self)
+	{
+		var queryParams = new List<string>();
+
+		foreach (var prop in self.GetType().GetProperties())
+		{
+			if (!FieldMappings.TryGetValue(prop.Name, out var key))
+				throw new InvalidOperationException($"Field '{prop.Name}' is not mapped to a query parameter.");
+
+			var value = prop.GetValue(self);
+
+			if (value is IEnumerable enumList && enumList.Cast<object>().All(x => x is Enum))
+			{
+				var values = string.Join(",", enumList.Cast<Enum>().Select(e => Convert.ToInt32(e)));
+				queryParams.Add($"{HttpUtility.UrlEncode(key)}={HttpUtility.UrlEncode(values)}");
+			}
+		}
+
+		return string.Join("&", queryParams);
+	}
+}
---
+++ PropertyDataTester/RealEstateApiClient.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace PropertyDataTester;
+
+public sealed class RealEstateApiClient
+{
+	private readonly HttpClient _httpClient;
+	private const string BaseUrl = "https://api-statements.tnet.ge/v1";
+
+	public RealEstateApiClient()
+	{
+		_httpClient = new HttpClient();
+		_httpClient.DefaultRequestHeaders.Add("X-Website-Key", "myhome");
+	}
+
+	public async Task<string> GetRealEstateStatementsAsync()
+	{
+		var endpoint = Path.Combine(BaseUrl, "statements");
+		//var query = new Dictionary<string, string>
+		//{
+		//	{ "deal_types", "1" }, //
+		//	{ "real_estate_types", "1" }, //
+		//	{ "cities", "1" },
+		//	{ "urbans", "38,40,41,42,43,44,45,46,47,101,28" },
+		//	{ "districts", "4" },
+		//	{ "currency_id", "2" },
+		//	{ "price_from", "10000" },
+		//	{ "price_to", "70000" },
+		//	{ "area_from", "40" },
+		//	{ "area_types", "1" },
+		//	{ "page", "1" }
+		//};
+		var request = new GetStatementsRequest(
+			[DealType.Sale],
+			[RealEstateType.Flat],
+			[Districts.VakeSaburtalo, Districts.IsaniSamgori]);
+
+		var queryString = request.ToQueryString();
+		var apiUrl = $"{endpoint}?{queryString}";
+		//var apiUrl2 = @"https://api-statements.tnet.ge/v1/statements?deal_types=1,2&real_estate_types=1&cities=1&urbans=38%2C40%2C41%2C42%2C43%2C44%2C45%2C46%2C47%2C101%2C28&districts=4&currency_id=2&price_from=10000&price_to=70000&area_from=40&area_types=1&page=1";
+
+		var response = await _httpClient.GetStringAsync(apiUrl);
+		return response;
+	}
+}
