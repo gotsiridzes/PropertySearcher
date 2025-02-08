983523ef08968eb3fa46525e7cb3619baa163d20
Saba Gotsiridze <sag@leneo.io>
2025-02-09 02:18:59 +0400

    ref

--- PropertyDataTester/RealEstateApiClient.cs
+++ PropertyDataTester/RealEstateApiClient.cs
@@ -28,45 +28,53 @@ public sealed class RealEstateApiClient
 			[BuildingStatus.Old],
 			OrderBy.Price.Asc, Page: 1);
 
-		var paginationInfo = await GetPaginationInfoAsync(request);
+		var paginationInfo = await GetPaginationInfoAsync();
 		Console.WriteLine("Need to process {0} pages and {1} records", paginationInfo.Data.LastPage,
 			paginationInfo.Data.Total);
 		var result = new List<RealEstateStatement>();
 
 		for (var page = paginationInfo.Data.Page; page <= paginationInfo.Data.LastPage; page++)
 		{
-			var pageData = await GetStatementsByPageAsync(request, page);
+			var pageData = await GetStatementsByPageAsync(page);
 			result.AddRange(pageData);
 			Console.WriteLine("Processing {0} page", page);
 		}
 
 		return result;
-	}
 
-	private async Task<IReadOnlyCollection<RealEstateStatement>> GetStatementsByPageAsync(GetStatementsRequest request,
-		int page)
-	{
-		request = request with { Page = page };
-		var response = await _httpClient.GetAsync($"{BaseUrl}/statements?{request.ToQueryString()}");
-		response.EnsureSuccessStatusCode();
+		async Task<IReadOnlyCollection<RealEstateStatement>> GetStatementsByPageAsync(
+			int page)
+		{
+			request = request with { Page = page };
+			var response = await _httpClient.GetAsync($"{BaseUrl}/statements?{request.ToQueryString()}");
+			response.EnsureSuccessStatusCode();
 
-		var statementsResponse =
-			JsonSerializer.Deserialize<GetStatementsResponse>(await response.Content.ReadAsStringAsync());
+			var statementsResponse =
+				JsonSerializer.Deserialize<GetStatementsResponse>(await response.Content.ReadAsStringAsync());
 
-		return statementsResponse?.Data.Statements ??
-		       throw new("Error occured during response mapping");
-	}
+			return statementsResponse?.Data.Statements ??
+			       throw new("Error occured during response mapping");
+		}
 
-	private async Task<GetStatementsCountResponse> GetPaginationInfoAsync(GetStatementBaseRequest request)
-	{
-		var req = new GetStatementsCountRequests(request.DealTypes, request.RealEstateTypes, request.Districts,
-			request.Currency, request.Price, request.Area, request.Owner, request.StatementPosition,
-			request.BuildingStatuses, request.Order, request.Page);
+		async Task<GetStatementsCountResponse> GetPaginationInfoAsync()
+		{
+			var req = new GetStatementsCountRequests(
+				request.DealTypes,
+				request.RealEstateTypes,
+				request.Districts,
+				request.Currency,
+				request.Price,
+				request.Area,
+				request.Owner,
+				request.StatementPosition,
+				request.BuildingStatuses,
+				request.Order, request.Page);
 
-		var response = await _httpClient.GetAsync($"{BaseUrl}/statements/count?{req.ToQueryString()}");
-		response.EnsureSuccessStatusCode();
+			var response = await _httpClient.GetAsync($"{BaseUrl}/statements/count?{req.ToQueryString()}");
+			response.EnsureSuccessStatusCode();
 
-		return JsonSerializer.Deserialize<GetStatementsCountResponse>(await response.Content.ReadAsStringAsync()) ??
-		       throw new("Error occured during response mapping");
+			return JsonSerializer.Deserialize<GetStatementsCountResponse>(await response.Content.ReadAsStringAsync()) ??
+			       throw new("Error occured during response mapping");
+		}
 	}
 }
