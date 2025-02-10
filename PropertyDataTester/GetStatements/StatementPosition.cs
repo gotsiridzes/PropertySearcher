a3b873e6854c227f9373c3f72b9c412e530b8028
Saba Gotsiridze <sag@leneo.io>
2025-02-10 23:33:04 +0400

    Fix statement position validation

--- PropertyDataTester/GetStatements/StatementPosition.cs
+++ PropertyDataTester/GetStatements/StatementPosition.cs
@@ -13,21 +13,6 @@ public record struct StatementPosition
 	public bool? NotLast { get; }
 	public bool? IsLast { get; }
 
-	public static StatementPosition Create(bool? notFirst, bool? notLast, bool? isLast)
-	{
-		Validate(notFirst, notLast, isLast);
-		return new(notFirst, notLast, isLast);
-	}
-
-	private static void Validate(bool? notFirst, bool? notLast, bool? isLast)
-	{
-		if (isLast == true && (notFirst == true || notLast == true))
-			throw new InvalidOperationException("'is_last' cannot be used with 'not_first' or 'not_last'.");
-
-		if ((notFirst == true && notLast == true) || (notFirst == true && isLast == true) || isLast == true)
-			return;
-
-		throw new InvalidOperationException(
-			"Invalid combination: Allowed (not_first, not_last), (not_first, is_last), or (is_last).");
-	}
+	public static StatementPosition Create(bool? notFirst, bool? notLast, bool? isLast) =>
+		new(notFirst, notLast, isLast);
 }
